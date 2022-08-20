dc67028aaa39867961bbfa083ecb26f787bebee3
Mike Snyman <mikesnyman0403@gmail.com>
2022-08-20 13:19:43 +0200

    no changes

---
+++ Unit/src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    public static int add(int number1, int number2){
+        return number1+number2;
+    }
+    public static int min(int number1,int number2){
+        return number1-number2;
+    }
+    public static int multiply(int number1,int number2){
+        return number1*number2;
+    }
+    public static int remainder(int number1,int number2){
+        return number1%number2;
+    }
+
+    public static int divide(int number1, int number2){
+        return number1/number2;
+    }
+}
